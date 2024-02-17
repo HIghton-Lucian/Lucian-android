ac96c40287c99f14da5c3c0da9ee22d6608ed4cb
GSM-Minseok <ms0818000@gmail.com>
2024-02-17 20:10:24 +0900

    feat: set typography

--- app/src/main/java/com/high_thon/lucian/core/MainActivity.kt
+++ app/src/main/java/com/high_thon/lucian/core/MainActivity.kt
@@ -3,11 +3,13 @@ package com.high_thon.lucian.core
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.high_thon.lucian.common.theme.LucianTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,11 +20,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LucianTheme { color, typo ->
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = color.WHITE
-                ) {
-                    Greeting("Android")
+                Column {
+                    Text(
+                        text = "루드림",
+                        style = typo.B20,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    Text(
+                        text = "루드림",
+                        style = typo.R20,
+                    )
                 }
             }
         }
